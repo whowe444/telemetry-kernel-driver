ad65d357ece1cf213c63c8e07795653f2a1c3dfe
Will Howe <willhowe235@gmail.com>
2025-12-31 15:18:43 -0500

    Add RabbitMQ interface

--- connectors/ipc_to_rabbit/src/main.rs
+++ connectors/ipc_to_rabbit/src/main.rs
@@ -1,9 +1,10 @@
+use lapin::{BasicProperties, options::*, types::FieldTable, Connection, ConnectionProperties};
 use posixmq::{OpenOptions, PosixMq};
-use std::time::Duration;
-use std::io;
 use std::env;
+use tokio;
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the queue name from command line arguments
     let args: Vec<String> = env::args().collect();
 
@@ -24,19 +25,42 @@ fn main() -> io::Result<()> {
     
     println!("Message Queue opened or created: {}", queue_name);
 
+    // Establish connection to RabbitMQ
+    let conn = Connection::connect("amqp://guest:guest@localhost:5672/", ConnectionProperties::default())
+        .await?;
+    let channel = conn.create_channel().await?;
+
+    // Declare a queue
+    let _rabbit_mq = channel
+        .queue_declare(
+            queue_name,           // Queue name
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
     // Read messages from the queue.
     loop {
         let mut buf = vec![0; mq.attributes().unwrap().max_msg_len];
         match mq.recv(&mut buf) {
             Ok((_priority, len)) => {
-                println!("Received message: {:?}", &buf[..len]);
+                println!("Received message: {:?}", String::from_utf8_lossy(&buf[..len]));
+                channel
+                    .basic_publish(
+                        "",                      // Default exchange (empty means "amq.direct")
+                        queue_name,              // Routing key is the queue name
+                        BasicPublishOptions::default(),
+                        &buf,      // Message body
+                        BasicProperties::default(),
+                    )
+                    .await?
+                    .await?;
+
+                println!("Message sent to RabbitMQ!");
             },
             Err(e) => {
                 eprintln!("Error receiving message: {}", e);
             }
         }
-        
-        // Optional: You can add a timeout or a sleep to avoid busy-waiting.
-        std::thread::sleep(Duration::from_secs(1));
     }
 }
